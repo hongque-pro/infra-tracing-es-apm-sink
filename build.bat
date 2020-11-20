6c999d90e7f665a102cf1cfdd4ae49c0d5c60ff8
endink <sharping@outlook.com>
2020-11-20 22:56:25 +0800

    update build cmd

--- build.bat
+++ build.bat
@@ -1,17 +1,25 @@
 @echo off
+set output=%~dp0output
+SET GOARCH=amd64
+
+echo output dir: %output%
+echo.
+if not exist %output% (
+		md %output%
+    )
+
 echo Compile for windows ...
-del /f /s /q %~dp0\output\*.*
+del /f /s /q %output%\*.*
 pushd %~dp0
-go build -ldflags="-w -s" -o "./output/tracing-sink-win.exe" -i ./main
+go build -ldflags="-w -s" -o "./output/tracing-sink-win.exe" -i ./main/
 
 echo.
 set /p linux=compile for linux? [y/n]:
 if "%linux%"=="y" (
 SET CGO_ENABLED=0
 SET GOOS=linux
-SET GOARCH=amd64
 echo Compile for linux amd64 ...
-go build -ldflags="-w -s" -o "./output/tracing-sink-linux" -i ./main
+go build -ldflags="-w -s" -o "./output/tracing-sink-linux" -i ./main/
 )
 
 echo.
